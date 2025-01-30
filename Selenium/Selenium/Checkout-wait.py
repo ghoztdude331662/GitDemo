b8ef2ea246c055487ef1b53afc5da10bce4efecb
rahul <rahulprasad331662@gmail.com>
2025-01-30 14:04:54 -0500

    2 files

--- Selenium/Selenium/Checkout-wait.py
+++ Selenium/Selenium/Checkout-wait.py
@@ -22,4 +22,4 @@ driver.find_element(By.XPATH,"//button[text()='Apply']").click()
 x=driver.find_element(By.CSS_SELECTOR,".promoInfo").text
 assert x=="Code applied ..!"
 
-#git branch
+#git branch save
